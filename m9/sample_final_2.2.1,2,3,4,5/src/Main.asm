a4d2846b5ae60c697e5dfc2168da060088d29f79
coalpha <22552895+coalpha@users.noreply.github.com>
2021-08-06 14:30:46 -0700

    sample finals finished
    three things left:
    1. Final Multiple Choice Section
    2. Final Programming Section
    3. Homework M9

--- m9/sample_final_2.2.1,2,3,4,5/src/Main.asm
+++ m9/sample_final_2.2.1,2,3,4,5/src/Main.asm
@@ -7,16 +7,9 @@ main:
    ror al,3
    nop ; a.
    mov al,0D4h
-   rol al,7
+   rol al,7 
    nop ; b.
-   stc
-   mov al,0D4h
-   rcl al,1
-   nop ; c.
-   stc
-   mov al,0D4h
-   rcr al,3
-   nop ; d.
+
    xor eax, eax
    ret
 end main
